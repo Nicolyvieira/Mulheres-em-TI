6c2561392bb468b717456dd26f13892ae0c98bf6
Nicoly Vieira <92948872+Nicolyvieira@users.noreply.github.com>
2022-05-13 14:58:05 -0300

    Add files via upload

---
+++ mulheres-na-tec/mulheres-na-tec/bibliografia.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bibliografia</title>
+    <link rel="sortcut icon" href="images/logo.png" type="image/icon-x"/>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="links">
+        <h1>Bibliografia</h1>
+        <h2>Fontes do primeiro tema:</h2>
+
+        <a href="https://www.napratica.org.br/opcoes-carreiras-em-tecnologia/">- 10 opções de carreira em tecnologia para quem quer trabalhar na área </a>
+        <br>
+        <a href="https://www.voitto.com.br/blog/artigo/desenvolvedor-back-end">- Desenvolvedor back-end </a>
+        <br>
+        <a href="https://www.ewally.com.br/blog/ajudando-sua-empresa/backend/">- Back-end: O que é, para que serve e quais suas linguagens?</a>
+        <br>
+        <a href="https://www.digitalhouse.com/br/blog/o-que-um-desenvolvedor-full-stack-precisa-saber-programacao/">- Guia do desenvolvedor full stack: habilidades, salário e mercado de trabalho</a>
+        <br>
+        <a href="https://www.proway.com.br/blog/dev-full-stack-o-que-e-isso">- DEV Full Stack? O que é isso?</a>
+        <br>
+        <a href="https://www.totvs.com/blog/developers/front-end/#:~:text=O%20Front%2Dend%20est%C3%A1%20muito,com%20a%20experi%C3%AAncia%20do%20usu%C3%A1rio">- Front-end: o que é e como funciona?</a>
+        <br>
+        <a href="https://www.alura.com.br/artigos/o-que-e-front-end-e-back-end?gclid=Cj0KCQjw06OTBhC_ARIsAAU1yOUsFZyU3opkHrftRLMMNqiAND81vhZQUyKBCb5G6Mag4y1dnRHaLskaAqrAEALw_wcB">- O que é front-end e back-end?</a>
+        <br>
+        <a href="https://blog.anhanguera.com/o-que-e-front-end-e-back-end/?utm_source=google&utm_medium=cpc&utm_campaign=AEDU-Presencial::L3::Display::Performance-Max::Cursos-LTV&gclid=Cj0KCQjw06OTBhC_ARIsAAU1yOVW2iD9uwopDKIjJoOoascRa0XuODiy1JwmbuDV7YFgxTnZRzNdmvAaAh2DEALw_wcB#">- O que é e qual é a diferença entre Front-End e Back-End?</a>
+        <br>
+        <a href="https://blog.revelo.com.br/contratar-desenvolvedor-front-end/?amp">- Saiba acertar na contratação do Desenvolvedor Front-End</a>
+        <br>
+        <a href="https://www.jornalcontabil.com.br/veja-quanto-ganha-um-desenvolvedor-front-end/?amp">- Veja quanto ganha um Desenvolvedor Front End!</a>
+        <br>
+        <a href="https://www.vagas.com.br/cargo/desenvolvedor-front-end">- Desenvolvedor Front-end</a>
+        <br>
+
+        <h2>Fontes do segundo tema:</h2>
+
+        <a href="https://blogs.ne10.uol.com.br/mundobit/2019/11/12/jovens-homens-e-brancos-dominam-a-area-de-ti-no-brasil-aponta-estudo/">- Jovens, homens e brancos dominam a área de TI no Brasil, aponta estudo</a>
+        <br>
+
+        <h2>Fontes do terceiro tema:</h2>
+
+        <a href="https://deskmanager.com.br/blog/mulheres-na-ti/#ixzz7Rf0dsE1R">- Mulheres na TI: os desafios para se firmar na área</a>
+        <br>
+        <a href="https://www.baguete.com.br/noticias/22/12/2021/mulheres-no-ti-na-pratica-a-teoria-e-outra">- Mulheres no TI: na prática, a teoria é outra</a>
+        <br>
+        
+        <h2>Fontes do quarto tema:</h2>
+
+        <a href="https://catracalivre.com.br/criatividade/15-projetos-de-tecnologia-comandado-por-mulheres-e-para-mulheres/">- 15 projetos de tecnologia comandado por mulheres e para mulheres</a>
+    </div>
+    
+    <div class="voltar">
+        <a href="index.php"> Voltar </a>
+    </div>
+
+</body>
+</html>
